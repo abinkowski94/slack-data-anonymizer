73c79baeaf381bc6c354b521a6c1629f3269d775
Augustyn Binkowski <augustyn.mikolaj@gmail.com>
2024-05-12 16:04:44 +0200

    Add element user id anonymization

--- src/SlackDataAnonymizer/Services/Anonymizers/ElementsAnonymizerService.cs
+++ src/SlackDataAnonymizer/Services/Anonymizers/ElementsAnonymizerService.cs
@@ -16,18 +16,38 @@ public class ElementsAnonymizerService(
             return value;
         }
 
-        value.Text = textAnonymizer.Anonymize(value.Text, sensitiveData);
+        AnonymizeUserId(value, sensitiveData);
+        AnonymizeText(value, sensitiveData);        
+        AnonymizeSubElements(value, sensitiveData);
 
+        return value;
+    }
+
+    private static void AnonymizeUserId(Element value, ISensitiveData sensitiveData)
+    {
+        if (value.UserId is null)
+        {
+            return;
+        }
+
+        value.UserId = sensitiveData.GetOrAddUser(value.UserId);
+    }
+
+    private void AnonymizeText(Element value, ISensitiveData sensitiveData)
+    {
+        value.Text = textAnonymizer.Anonymize(value.Text, sensitiveData);
+    }
+
+    private void AnonymizeSubElements(Element value, ISensitiveData sensitiveData)
+    {
         if (value.Elements is null)
         {
-            return value;
+            return;
         }
 
         foreach (var subElement in value.Elements)
         {
             Anonymize(subElement, sensitiveData);
         }
-
-        return value;
     }
 }
